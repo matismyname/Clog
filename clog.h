f815f5a71506b9c996edff00b998ac000977f04e
mat <35172387+matismyname@users.noreply.github.com>
2024-01-28 22:39:24 +0100

    Update clog.h
    Updated clog.h with the new functions in clog.c

--- clog.h
+++ clog.h
@@ -1,10 +1,18 @@
 #ifndef _CLOG_H_
 #define _CLOG_H_
 
-void Clog(char* msg, char* log_level);
-static char* Create_Log_Directory();
-static void Write_Log(char* msg, char* log_dir, char* log_level);
-void Delete_Log(char* log_level);
-void Delete_Logs();
+void Clog(const char* msg, const char* log_level);
+void Clog_char(const char* fmsg, const char* smsg, const char* log_level);
+void Clog_int(const char* msg, int d, const char* log_level);
+void Clog_float(const char* msg, float f, const char* log_level);
+void Clog_double(const char* msg, double dd, const char* log_level);
+const char* CreateLogDirectory(void);
+void WriteLog(const char* msg, const char* log_dir, const char* log_level);
+void DeleteLog(const char* log_level);
+void DeleteLogs(void);
+const char* CombineMessages_char(const char* msg1, const char* msg2);
+const char* CombineMessages_int(const char* msg1, int d);
+const char* CombineMessages_float(const char* msg1, float f);
+const char* CombineMessages_double(const char* msg1, double dd);
 
 #endif
